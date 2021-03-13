696862d6f62a5da351ea3d21cfcea7d5bf89c763
DinhSang <dinhsangdroid@gmail.com>
2021-03-13 10:48:16 +0700

    Firebase-DinhSang: create, connect android app and firebase

--- app/src/main/java/com/example/android_firebase/MainActivity.java
+++ app/src/main/java/com/example/android_firebase/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("Hello, World!");
     }
 }
